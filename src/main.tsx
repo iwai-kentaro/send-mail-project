c00509ac08d8f0ad376e3c7ad03369269851a68a
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-02-01 13:45:40 +0900

    insert追加

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider>
+      <SpeedInsights />
       <App />
     </ChakraProvider>
   </StrictMode>
