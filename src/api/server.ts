0cd5c59b035c1ea94f64387caff30f7953e3573f
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-02-01 22:23:52 +0900

    log確認

--- src/api/server.ts
+++ src/api/server.ts
@@ -10,6 +10,8 @@ dotenv.config();
 const PORT = process.env.PORT || 5001;
 const HOST = process.env.HOST || "https://send-mail-project-kentaro.vercel.app";
 const app = express();
+console.log(app);
+
 
 // ✅ CORS 設定（`bodyParser.json()` より前に定義）
 app.use(
